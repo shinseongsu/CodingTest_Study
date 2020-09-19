ba3e9b16447e943fc1a4ce080e67ecca3bba76ce
Shin seongsu <tlstjdtn321@naver.com>
2020-09-19 23:35:09 +0900

    백준 개그림 그리기

---
+++ src/BackJoon/Level1/Dog.java
@@ -0,0 +1,27 @@
+package BackJoon.Level1;
+
+/**
+ * 개
+ *
+ * 문제
+ * 아래 예제와 같이 개를 출력하시오.
+ *
+ * 출력
+ * 개를 출력한다.
+ *
+ * |\_/|
+ * |q p|   /}
+ * ( 0 )"""\
+ * |"^"`    |
+ * ||_/=\\__|
+ */
+
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
