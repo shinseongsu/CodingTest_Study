3e3679273e078ca51a9704805cb1db6a651937e1
Shin seongsu <tlstjdtn321@naver.com>
2020-09-19 23:35:37 +0900

    백준 Hello World! 출력하기

---
+++ src/BackJoon/Level1/HelloWorld.java
@@ -0,0 +1,19 @@
+package BackJoon.Level1;
+
+/**
+ * Hello World
+ *
+ * 문제
+ * Hello World!를 출력하시오.
+ *
+ * 출력
+ * Hello World!를 출력하시오.
+ *
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
