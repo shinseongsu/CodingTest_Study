94ef9bbf3e5b472858c262c0f7fdbc5805579e42
Shin seongsu <tlstjdtn321@naver.com>
2020-10-01 19:02:47 +0900

    기찍 N

---
+++ src/BackJoon/Level3/NPrintDesc.java
@@ -0,0 +1,38 @@
+package BackJoon.Level3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * 기찍 N
+ *
+ * 문제
+ * 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+ *
+ * 출력
+ * 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+ *
+ * 예제 입력 1
+ * 5
+ * 예제 출력 1
+ * 5
+ * 4
+ * 3
+ * 2
+ * 1
+ *
+ */
+public class NPrintDesc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = N ; i >= 1 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
