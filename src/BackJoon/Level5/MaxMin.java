eb5257f7b4fc25c7112d0014b6dadf4b5ed363cd
Shin seongsu <tlstjdtn321@naver.com>
2020-10-02 18:16:41 +0900

    최소, 최대

---
+++ src/BackJoon/Level5/MaxMin.java
@@ -0,0 +1,48 @@
+package BackJoon.Level5;
+
+import java.util.Scanner;
+
+/**
+ * 최소, 최대
+ *
+ * 문제
+ * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다.
+ * 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다.
+ * 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+ *
+ * 출력
+ * 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+ *
+ *
+ * 예제 입력 1
+ * 5
+ * 20 10 35 30 7
+ * 예제 출력 1
+ * 7 35
+ *
+ */
+public class MaxMin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int min = 1000000;
+        int max = 1;
+
+        int number = sc.nextInt();
+
+        for(int i = 0 ; i < number ; i++) {
+            int base_number = sc.nextInt();
+
+            min = Math.min(base_number, min);
+            max = Math.max(base_number, max);
+        }
+
+        System.out.println(min);
+        System.out.println(max);
+
+
+    }
+}
